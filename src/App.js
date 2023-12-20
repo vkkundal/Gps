358ea8c38091a478562998af20f0dde0650fe8dc
vkkundal <vinodkumar1805@gmail.com>
2023-12-20 21:21:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
+  const [userAddress, setUserAddress] = useState();
+
+  console.log("latitude:", lat, lon);
+  const geo = navigator.geolocation
+  const watchID = geo.watchPosition(userCoords)
+  function userCoords(position) {
+    let userlat = position.coords.latitude
+    let userlon = position.coords.longitude
+    setLat(userlat);
+    setLon(userlon);
+  }
+  const getUserAddress = async () => {
+    let url = `https://api.opencagedata.com/geocode/v1/json?key=ed693e08c1f7453e8083381cc90004b5&q=${lat}%2C+${lon}&pretty=1&no_annotations=1`
+    const loc = await fetch(url)
+    const data = await loc.json()
+    console.log("user address -", data);
+    setUserAddress(data.results[0].formatted)
+  }
+  const handleGetUserAddress = () => {
+    getUserAddress()
+  }
+
+  const stopGPS = () => {
+    geo.clearWatch(watchID)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1> Current location</h1>
+        <h1> latitude -{lat}</h1>
+        <h1> longitude -{lon}</h1>
+        <h1> user Address -{userAddress}</h1>
+        <button onClick={handleGetUserAddress}>Get user Address</button><br/>
+        <button onClick={stopGPS}>Stop Gps</button>
+      </div>
+    </>
+
   );
 }
 
